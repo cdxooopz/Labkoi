28d8b631dbc00e512977dd06f689340febab3d63
Bever <cdxooopz@gmail.com>
2017-10-09 09:42:14 +0700

    Delete Analytics

--- backoffice/ck_plugin/templates/inc_footer.php
+++ backoffice/ck_plugin/templates/inc_footer.php
@@ -129,12 +129,3 @@
 			}
 		?>
 		<!-- End console -->
-		<!-- Global Site Tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-90668382-1"></script>
-		<script>
-		  window.dataLayer = window.dataLayer || [];
-		  function gtag(){dataLayer.push(arguments)};
-		  gtag('js', new Date());
-		
-		  gtag('config', 'UA-90668382-1');
-		</script>
